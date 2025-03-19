08b3a4effc24900d6ac44f37083005b80bb28d22
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-03-19 13:41:38 +0800

    Create Swap Nodes in Pairs.cpp

---
+++ Swap Nodes in Pairs.cpp
@@ -0,0 +1,26 @@
+#Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        while (head && head->next) {
+            ListNode* first = head;
+            ListNode* second = head->next;
+
+           
+            prev->next = second;
+            first->next = second->next;
+            second->next = first;
+
+            
+            prev = first;
+            head = first->next;
+        }
+
+        return dummy.next;
+    }
+};
