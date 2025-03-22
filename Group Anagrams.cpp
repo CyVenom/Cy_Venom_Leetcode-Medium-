1000b1918a8916d241f2dfb8af2d45a3fd354900
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-03-22 20:02:25 +0800

    Create  Group Anagrams.cpp

---
+++ Group Anagrams.cpp
@@ -0,0 +1,23 @@
+#Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramMap;
+        
+      
+        for (string& str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end()); 
+            anagramMap[sortedStr].push_back(str);
+        }
+
+        
+        vector<vector<string>> result;
+        for (auto& entry : anagramMap) {
+            result.push_back(entry.second);
+        }
+        
+        return result;
+    }
+};
